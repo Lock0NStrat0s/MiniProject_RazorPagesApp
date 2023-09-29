7f486d9d170bab2a77f77e924fd0baab11bf76b2
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-29 00:38:03 -0600

    Add project files.

---
+++ MiniProject_RazorPages/Pages/AddressEntry.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MiniProject_RazorPages.Pages
+{
+    public class AddressEntryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MiniProject_RazorPages/Pages/PersonEntry.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MiniProject_RazorPages.Pages
+{
+    public class PersonEntryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
