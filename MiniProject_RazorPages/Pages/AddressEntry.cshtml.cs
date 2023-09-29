b307afacd1199d797585f65507dc3c3949898ad7
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-29 01:11:14 -0600

    mini project complete.

--- MiniProject_RazorPages/Pages/AddressEntry.cshtml.cs
+++ MiniProject_RazorPages/Pages/AddressEntry.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WinFormsDemoLibrary;
 
 namespace MiniProject_RazorPages.Pages
 {
     public class AddressEntryModel : PageModel
     {
+        [BindProperty]
+        public AddressModel Address { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("./Index");
+            }
         }
     }
 }
--- MiniProject_RazorPages/Pages/PersonEntry.cshtml.cs
+++ MiniProject_RazorPages/Pages/PersonEntry.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WinFormsDemoLibrary;
 
 namespace MiniProject_RazorPages.Pages
 {
     public class PersonEntryModel : PageModel
     {
+        [BindProperty]
+        public PersonModel Person { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("./Index");
+            }
         }
     }
 }
